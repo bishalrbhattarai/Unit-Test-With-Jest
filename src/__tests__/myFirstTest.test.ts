f2d10b4837d6797964590434bbaa1e47b76fc5aa
Bishal Raj Bhattarai <bishalrajbhattarai01@gmail.com>
2024-12-07 18:35:21 +0545

    reference type unit test

--- src/__tests__/myFirstTest.test.ts
+++ src/__tests__/myFirstTest.test.ts
@@ -1,3 +1,19 @@
 test("1 + 1 should be 2 (Two)", () => {
   expect(1 + 1).toBe(2);
 });
+
+test("returns object with name and age", () => {
+  const obj = {
+    name: "Bishal",
+    age: 25,
+  };
+  expect(obj).toMatchObject({
+    name: "Bishal",
+    age: 25,
+  });
+
+  expect(obj).toEqual({
+    name: expect.any(String),
+    age: expect.any(Number),
+  });
+});
